aac96945af2e495fddbb86cdeb7f60d2ccc3fb92
Muhammad Fakhar Sultan <umerfakhar5@gmail.com>
2024-09-05 20:10:29 +0500

    Fix route

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const controller = require("../controllers/postController");
 const { validateObjectIdMiddleware } = require("../middlewares/objectIdMiddleware");
-const { validatePostMiddleware } = require("../middlewares/postMiddleware");
 
 const router = express.Router();
 
 router.get("/" , controller.getAllPosts);
 
 router.get("/:id", validateObjectIdMiddleware , controller.getSinglePost);
-
-router.post("/", validatePostMiddleware, controller.addNewPost);
 
 module.exports = router;
